101561a8c05a6f71552fe35d1959ea0892e319a3
Shashidhar HB <hb.shashi@gmail.com>
2013-09-19 21:15:14 +0530

    First Hadoop Word Count

---
+++ wordcount-mapred/src/main/java/com/netapp/learn/WordCount.java
@@ -0,0 +1,82 @@
+package com.netapp.learn;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+
+public class WordCount extends Configured implements Tool {
+
+
+	public static void main(String[] args) throws Exception {
+		ToolRunner.run(new WordCount(), args);
+	}
+
+
+	public static class Map extends Mapper <LongWritable,Text,Text,IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			//StringTokenizer tokenizer = new StringTokenizer(line);
+
+			String[] array = line.split("\\s+");
+			for (String string : array) {
+				word.set(string.trim());
+				context.write(word, one);
+
+			}
+		}
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable intWritable : values) {
+				sum += intWritable.get();
+			}
+			context.write(key, new IntWritable(sum));			
+		}
+	}
+
+
+	@Override
+	public int run(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: wordcount <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+
+}
